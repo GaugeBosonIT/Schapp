4015efaf4f5891e681a2147f097ebbfaed637ad2
Harry McCarney <hmccarney@gmail.com>
2011-10-12 13:25:41 +0200

    initial commit with api proof of concept

---
+++ Schapp/Resources/List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Schapp.Resources
+{
+    public class List
+    {
+        public string Name { get; set; }
+    }
+}
