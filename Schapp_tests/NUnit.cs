6d32643a39277c437bed7e2f7f92d790065d4b2b
Harry McCarney <hmccarney@gmail.com>
2011-11-25 19:48:35 +0100

    added unit test project

---
+++ Schapp_tests/NUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Schapp.Controllers;
+
+namespace AudioTag
+{
+    [TestFixture]
+    public class NUnit
+    {
+        [Test]
+        public void Index_action_returns_a_view()
+        {
+            //arrange
+            var c = new HomeController();
+            //act
+            var i = c.Index();
+            //assert
+            Assert.AreEqual("", "", "Index view wrongly named");
+
+
+
+        }
+
+    }
+}
