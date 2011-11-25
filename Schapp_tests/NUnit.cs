8ea16b57f72a716b24ca55eeeafbda0592614c9d
Harry McCarney <hmccarney@gmail.com>
2011-11-26 00:00:45 +0100

    added js testing to hudson

--- Schapp_tests/NUnit.cs
+++ Schapp_tests/NUnit.cs
@@ -18,7 +18,7 @@ namespace AudioTag
             //act
             var i = c.Index();
             //assert
-            Assert.AreEqual("testing1", "testing1", "Index view wrongly named");
+            Assert.AreEqual("testing2", "testing2", "Index view wrongly named");
 
 
 
